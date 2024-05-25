345734148beaac845bd90b63e65ad3ee1ceb6be1
gonchigars <gonchigars@gmail.com>
2024-05-25 14:24:12 +0530

    first commit

---
+++ day1/demo/src/test/java/com/example/MainTest.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testMainPrintsHelloWorld() {
+        Main.main(null); // Call the main method
+        String actualOutput = outContent.toString().replace("\r\n", "\n");
+        assertEquals("Hello world!\n", actualOutput);
+    }
+}
